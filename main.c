b5850786aa3bb99fcf5a4fbb6cf1545d82137b19
jana elnagey <janaelnageyy@gmail.com>
2025-12-10 13:30:54 +0200

    main after changing

--- main.c
+++ main.c
@@ -5,18 +5,21 @@
 
 int main(){
     
-    char board[10][10];char move[6]; int a[4];int count=0;char disp[32];
+    char board[10][10];char move[6]; int a[4]; int currentPlayer=0;int count=0;char disp[32];
     initilize(board);
     printBoard(board);
     while(1){
-    scanf("%s",move);
-    change(move,a);
-    moving(board,a);
-    printBoard(board);}
-
-    
-
-
+        printf("player %d turn: ",currentPlayer+1);
+        scanf("%s",move);
+        change(move,a);
+        if (isValidMove(board, a, currentPlayer)) {
+            currentPlayer = 1 - currentPlayer; // Switching between players
+            moving(board,a);
+            printBoard(board);}
+        else {
+            printf("Invalid move, try again.\n");
+        }
+    }
     return 0;
 
 }
